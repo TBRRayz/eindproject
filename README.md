1e3c943158f9e61e7c0288c6378a5c54a7bd6458
Lars Broer <0916364@hr.nl>
2017-05-25 12:29:18 +0200

    First review vers. checking readme

--- README.md
+++ README.md
@@ -65,6 +65,32 @@ de besturing voor de tanks op een manier dat de voorkant ook de voorkant blijft 
 - colision
 
 # feedback week 4
+Probeer voor jezelf wat duidelijkheid te creëren tijdens het programmeren. De code ziet er goed uit, je kunt goed zien dat je weet waar je mee bezig bent.
+
+Echter staan (stonden) er hier en daar wat 'loze' comments voor functionaliteiten die je hebt gemaakt op een andere plek. Probeer dit soort dingen bij te houden, ziet je code er overzichtelijk uit. Uitlijning en consistentie is belangrijk.
+
+In je UML kon ik maar één tank vinden, maar toch heb je twee tank classes die eigenlijk exact het zelfde doen maar op een andere positie. Tank 1 maakte gebruik van een interface en tank 2 extends van gameObject. Ik heb voor consistentie (en je UML gevolgd) hier beide een extensie van gameObject voor gemaakt. Maar kijk naar de mogelijkheden om maar 1 tank class te hebben. Scheelt veel code die je moet herhalen. 
+
+## Beoordeling
+[x] De code voor het deelproduct staat op je eigen github
+[x] Er is een live page waar de game speelbaar is
+[x] Het deelproduct moet werkend zijn zonder foutmeldingen / bugs
+[x] Het deelproduct bevat een Readme (evt. met installatie instructies)
+[x] Er is een klassendiagram aanwezig
+[x] Je legt uit in je Readme waar je gebruik maakt van de volgende programmeerprincipes
+    [x] Interface
+    [x] Static Utility Method
+    [x] Singleton
+    [x] Strategy
+    [x] Een van de programmeertechnieken uit jaar 1:
+        [x] Encapsulation
+        [x] Inheritance
+
+Opmerkingen:
+Je bent heel erg goed op weg naar het eindproduct. Ik mis nog een strategy pattern. In je UML kon ik deze ook niet zo snel vinden.
+Een tip vanuit mij zou zijn om een tankbehavior te maken waar je een idle behavior hebt en een reloading. En dat je een functionaliteit toevoegd waardoor een tank x aantal bullets heeft. Als deze op zijn je moet reloaden en in een behavior terecht komt waar je 1 of 2 seconden stilstaat (of niet kunt schieten) of iets dergelijks.
+
+Resultaat: Voldoende
 
 
 
--- dev/tank1.ts
+++ dev/tank1.ts
@@ -1,72 +1,52 @@
-/// <reference path="tank.ts" />
+/// <reference path="gameObject.ts" />
 
 
-class Tank1 implements Tank {
+class Tank1 extends gameObject {
 
-    private speed:number;
+    private speed       :   number;
     //private div:HTMLElement;
-    private directionX: number = 0;
-    private directionY: number = 0;
-    
-    public x:number;
-    public y:number;
-    public tag:string;
-    public div:HTMLElement;
-    
+    private directionX  :   number = 0;
+    private directionY  :   number = 0;
     
     //public orientation: Vector = new Vector(1, 0);
     //public pointList: Array<Vector> = new Array<Vector>();
     //public rotation: number = 0;
     
-    public size: number = 20;
-    
-            
-    constructor(x:number, y:number, tag:string) {
-        
-        
+    public size         :   number = 20;
+              
+    constructor() {
+        super("tank1");
+          
         this.directionX = 0;
         this.directionY = 0;
         
-        
-
-        this.speed = 4;
-        this.x = x;
-        this.y = y;
-        this.tag = tag;
-
-        let container:HTMLElement = document.getElementById("container");
-        
-        this.div = document.createElement(tag);
-        container.appendChild(this.div);
-
+        this.speed      = 4;
+        this.x          = 100;
+        this.y          = 200;
         
         window.addEventListener("keydown", (e) => this.onKeyDown(e));
         window.addEventListener("keyup"  , (e) => this.onKeyUp(e));
-        
     }
+
     private onKeyDown(event:KeyboardEvent):void {
         switch(event.keyCode){
-            
-            case 39: //RIGHT
+            case 39: //Right
                 //this.turnRight();
                 break;
-                
-            case 40: //RIGHT
+            case 40: //Down
                 this.directionY = 1;
                 break;
-            case 38: //RIGHT
+            case 38: //Up
                 this.directionY = -1;
                 break;
-            
-            case 37: //LEFT
+            case 37: //Left
                 //this.turnLeft();
                 break;
         }
-        
     }
+
      private onKeyUp(event:KeyboardEvent):void {
         switch(event.keyCode){
-           
             case 39: //RIGHT
                 this.directionX = 0;
                 break;
@@ -105,22 +85,15 @@ class Tank1 implements Tank {
     public move() : void {
         this.x = this.x + this.speed * this.directionX;
         this.y = this.y + this.speed * this.directionY;
-    
+
+        // Comment: Op dit moment beweegt je tank uit het scherm, probeer iets als:
+        // if (this.y > 20 && this.y < document.getElementById("container").clientWidth - 50) {
+        //     this.y = this.y + this.speed * this.directionY;
+        // }
     }
     
     public draw() : void {
         this.div.style.transform = "translate("+this.x+"px, "+this.y+"px)";
-        
-        
-    }
-
-    
-    
-   
-    
-    
-
-    //
-    // hier een method maken voor on key press
-    //
+         
+    }  
 }
